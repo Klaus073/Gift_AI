02bbdfb32a6c9d528dbe40f0550b4748d2d1f91a
Void <65257448+Klaus073@users.noreply.github.com>
2023-11-10 17:15:27 +0500

    removed code to render index.html

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 # Import your code and functions here
 from chatbot import main_input
  
-@app.route('/')
-def index():
-    return render_template('index.html')
+
  
 @app.route('/api/main_input', methods=['POST'])
 def api_main_input():
