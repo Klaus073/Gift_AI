cc8806d76c1f229f77ec12963a5bfa2cac75d4e2
Rao Nauman <nomirao1254@gmail.com>
2023-11-22 22:34:21 +0500

    moved resolved changes of cache file

--- cache_service.py
+++ cache_service.py
@@ -7,6 +7,20 @@ from paapi5_python_sdk.models.search_items_request import SearchItemsRequest
 # Initialize Redis connection
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+def simplify_json(json_obj):
+    result = {}
+
+    def flatten(obj, prefix=""):
+        if isinstance(obj, dict):
+            for key, value in obj.items():
+                new_key = f"{prefix}_{key}" if prefix else key
+                flatten(value, new_key)
+        else:
+            result[prefix] = obj
+
+    flatten(json_obj)
+    return result
+
 def get_cached_response(search_items_params, send_req):
     # Generate a unique cache key from the request parameters
     keys_to_use = search_items_params.keywords.lower() + search_items_params.search_index.lower()
@@ -15,25 +29,18 @@ def get_cached_response(search_items_params, send_req):
     # Check if the response is already in the cache
     cached_response = redis_client.get(cache_key)
     if cached_response:
-        print("Cached Response:")
-        print(json.loads(cached_response))
         return json.loads(cached_response)
     else:
         # NOTE -- Make the request
         response = send_req(search_items_params)
+        
 
-        print('response',response)
+        serialized_res = simplify_json(response)
+        api_output=serialized_res[""]
+        api_output_dict= api_output.to_dict()
+
         # Cache the response in Redis with a TTL (time-to-live)
-        redis_client.set(cache_key, json.dumps(response), 3600) # Cache for 1 hour
+        redis_client.set(cache_key, json.dumps(api_output_dict), 3600) # Cache for 1 hour
 
         # Print the response
-        print("API Response:")
-        print(response)
-        return response
-
-
-# def test(asd):
-#     print("sending request")
-#     return dict(text= "hello world", random_field= "Hola there")
-
-# print(get_cached_response(dict(keywords= "harry potter", search_index="Books"), test))
+        return api_output_dict
